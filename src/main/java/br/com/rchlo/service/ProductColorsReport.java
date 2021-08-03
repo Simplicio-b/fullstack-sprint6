de46fac88d88584933d80e3832f87438703e3903
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-08-03 07:06:34 -0300

    Relatório de quantidade de produtos por cor

---
+++ src/main/java/br/com/rchlo/service/ProductColorsReport.java
@@ -0,0 +1,41 @@
+package br.com.rchlo.service;
+
+import br.com.rchlo.domain.Color;
+import br.com.rchlo.domain.Product;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductColorsReport {
+
+    private Map<Color, Integer> colorCount = new HashMap<Color, Integer>();
+
+    public ProductColorsReport() {
+        this.colorCount.put(Color.BLUE, 0);
+        this.colorCount.put(Color.GRAY, 0);
+        this.colorCount.put(Color.GREEN, 0);
+        this.colorCount.put(Color.PINK, 0);
+        this.colorCount.put(Color.RED, 0);
+        this.colorCount.put(Color.WHITE, 0);
+    }
+
+    public Map<Color, Integer> report(List<Product> products) {
+        for (Product p: products) {
+            if (this.colorCount.containsKey(p.getColor())) {
+                int novaQtdDeProdutosCor = this.colorCount.get(p.getColor()) + 1;
+                this.colorCount.put(
+                        p.getColor(),
+                        novaQtdDeProdutosCor
+                );
+            }
+        }
+
+        return this.colorCount;
+    }
+
+    public void all() {
+        System.out.println(this.colorCount);
+    }
+
+}
