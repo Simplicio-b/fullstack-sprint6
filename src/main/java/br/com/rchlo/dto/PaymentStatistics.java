184875b79fba9d6a73b19855a7816f57ee5430e0
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-08-04 12:34:31 -0300

    Favorecendo composição à herança no cálculo de estatísticas de pagamento

--- src/main/java/br/com/rchlo/dto/PaymentStatistics.java
+++ src/main/java/br/com/rchlo/dto/PaymentStatistics.java
@@ -7,9 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-public class PaymentStatistics extends HashMap<PaymentStatus, Long> {
+public class PaymentStatistics {
 
     private BigDecimal maximumAmountOfConfirmedPayment;
+    private HashMap<PaymentStatus, Long> paymentStatus;
 
     public PaymentStatistics(BigDecimal maximumAmountOfConfirmedPayment) {
         this.maximumAmountOfConfirmedPayment = maximumAmountOfConfirmedPayment;
@@ -20,42 +21,17 @@ public class PaymentStatistics extends HashMap<PaymentStatus, Long> {
     }
 
     public Map<PaymentStatus, Long> getQuantityOfPaymentsByStatus() {
-        return this;
+        return paymentStatus;
     }
 
     public void addPaymentForStatus(PaymentStatus status) {
-        Long quantity = this.get(status);
+        Long quantity = this.paymentStatus.get(status);
         if (quantity == null) {
             quantity = 1L;
         } else {
             quantity++;
         }
-        this.put(status, quantity);
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(Object key, Object value) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean replace(PaymentStatus key, Long oldValue, Long newValue) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void putAll(Map<? extends PaymentStatus, ? extends Long> m) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Long merge(PaymentStatus key, Long value, BiFunction<? super Long, ? super Long, ? extends Long> remappingFunction) {
-        throw new UnsupportedOperationException();
+        this.paymentStatus.put(status, quantity);
     }
 
 }
